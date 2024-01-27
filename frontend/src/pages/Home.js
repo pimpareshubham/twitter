c6185dfc02e5387464fb29535e2a28b0012b4109
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2024-01-27 13:27:42 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import Feed from '../components/Feed';
-import './Home.css';
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home() {
-  const [sidebarVisible, setSidebarVisible] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarVisible(!sidebarVisible);
-  };
-
-  return (
-    <div className={`d-flex flex-row ${sidebarVisible ? 'toggled' : ''}`}>
-      <Sidebar />
-      <Feed />
-      <button
-        className="btn btn-primary d-none d-md-block btn-toggle-sidebar"
-        onClick={toggleSidebar}
-      >
-        Toggle Sidebar
-      </button>
-    </div>
-  );
+    return (
+        <div className='d-flex flex-row'>
+            <Sidebar className="col-12" />
+            <Feed className="col-12"/>
+        </div>
+    )
 }
