e6c8472a614141e4d7a2485f23954fdd3b06afa1
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2024-01-27 13:31:36 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,8 +5,8 @@ import Feed from '../components/Feed'
 export default function Home() {
     return (
         <div className='d-flex flex-row'>
-            <Sidebar className="col-12" />
-            <Feed className="col-12"/>
+            <Sidebar />
+            <Feed />
         </div>
     )
 }
